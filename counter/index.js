96020e98ba3be9cc4553f70468be8ba5d8ff7311
William Delaosa <wdelaosa24@gmail.com>
2025-05-03 12:22:29 -0400

    Initial commit

---
+++ counter/index.js
@@ -0,0 +1,27 @@
+// document.getElementById("count-el").innerText = 5;
+
+// let firstBatch = 5
+// let secondBatch = 2;
+
+// let count = firstBatch + secondBatch;
+
+// console.log(count);
+
+
+
+// let myAge = 19;
+// let humanDogRatio = 7;
+// let myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge);
+
+let bonusPoints = 50;
+console.log(bonusPoints)
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints);
+bonusPoints = bonusPoints - 75;
+console.log(bonusPoints);
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
+
+// let myAge = 19
+// console.log(myAge);
